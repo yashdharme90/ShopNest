39ba4ea6b01eeab1144a8ba7a6b97f5f3f5cd728
Yash Dharme <yashdharme0414@gmail.com>
2026-07-13 11:37:37 +0530

    Add files via upload

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,29 +1,3 @@
-// const nodemailer = require('nodemailer');
-
-// const sendEmail = async (to, subject, text) =>{
-//     try{
-//         const transporter =nodemailer.createTransport({
-//             service: 'Gmail',
-//             auth:{
-//                 user: process.env.EMAIL_USER,
-//                 pass: process.env.EMAIL_PASS
-//             }
-//         });
-//         const mailOptions = {
-//             from: process.env.EMAIL_USER,
-//             to,
-//             subject,
-//             text
-//         };
-//         await transporter.sendMail(mailOptions);
-//     }
-//     catch(error){
-//         console.log('Error sending email',error);
-//     }
-// };
-
-// module.exports = sendEmail;
-
 const nodemailer = require('nodemailer');
 const sendEmail = async ({ email, subject, message }) => {
     try {
